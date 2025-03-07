3ac6738e21e75353efa5f5398de56ec4907bac4c
Ryan Glover <ersatzryan@gmail.com>
2025-03-07 00:19:31 -0500

    Add ability to get a flow by id

--- lib/activepieces/resources/flows.rb
+++ lib/activepieces/resources/flows.rb
@@ -4,5 +4,10 @@ module Activepieces
       response = get("flows", params)
       Collection.from_response(response, type: Flow)
     end
+
+    def find(id)
+      response = get("flows/#{id}")
+      Flow.new(**response.body)
+    end
   end
 end
--- spec/activepieces/resources/flows_spec.rb
+++ spec/activepieces/resources/flows_spec.rb
@@ -15,4 +15,15 @@ RSpec.describe Activepieces::FlowsResource do
       expect(item.displayName).to eq("My Awesome Flow")
     end
   end
+
+  describe "#find" do
+    it "returns a flow" do
+      stub = stub_request("flows/1", response: stub_response(fixture: "flows/get"))
+      client = Activepieces::Client.new(stubs: stub)
+      flow = client.flows.find(1)
+
+      expect(flow).to be_a(Activepieces::Flow)
+      expect(flow.displayName).to eq("My Other Awesome Flow")
+    end
+  end
 end
